413e9f9ef427ea35724c6ae573036b268bb9f918
Yatoute <mintoamayatoute@gmail.com>
2025-07-16 10:10:28 +0200

    MISC : Installation du DSL de Kafka streams pour scala

--- src/main/scala/App_streaming.scala
+++ src/main/scala/App_streaming.scala
@@ -5,7 +5,8 @@ import org.apache.kafka.streams.scala.kstream.KGroupedStream
 import org.apache.kafka.streams.scala.kstream.KGroupedTable
 import org.apache.kafka.streams.scala.kstream.SessionWindowedKStream
 import org.apache.kafka.streams.scala.kstream.TimeWindowedKStream
-import org.apache.kafka.streams.scala.ImplicitConversions
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.state._
 import org.apache.kafka.streams.scala.Serdes
 
 
